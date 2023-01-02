0bc6a90f5f43096b4ed38e04a8955c9e22ef74ce
colindharrington <colindavidharrington@gmail.com>
2023-01-02 09:11:14 -0800

    vercel help

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,5 +1,13 @@
 import "../styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
+
+export default MyApp;
