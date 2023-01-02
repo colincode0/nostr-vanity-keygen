2a9c790a18490728b0d8d92ba35fe843858f815e
colindharrington <colindavidharrington@gmail.com>
2023-01-02 09:18:08 -0800

    New Next vercel format

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,13 +1,6 @@
 import "../styles/globals.css";
 import { Analytics } from "@vercel/analytics/react";
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  );
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp;
--- app/pages/index.js
+++ app/pages/index.js
@@ -4,6 +4,8 @@ import { Inter } from "@next/font/google";
 import styles from "../styles/Home.module.css";
 import HomePage from "../components/HomePage";
 import KeyGen from "../components/KeyGen";
+import { AnalyticsWrapper } from "../components/analytics";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -15,6 +17,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <KeyGen />
+      <AnalyticsWrapper />
       {/* <main className={styles.main}>
         <div>
           <HomePage />
